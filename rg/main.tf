22dfbe2d9d57b277d2227302106b762f37d2e971
omsaravanan1988 <91906734+omsaravanan1988@users.noreply.github.com>
2025-06-28 15:28:07 +0530

    Update main.tf

--- rg/main.tf
+++ rg/main.tf
@@ -11,7 +11,7 @@ resource "azurerm_virtual_network" "example" {
   name                = "example-vnet"
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = var.vnetprefix
 }
 
 # Configure the Subnet
@@ -19,7 +19,7 @@ resource "azurerm_subnet" "example" {
   name                 = "example-subnet"
   resource_group_name  = azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = var.subnetprefix
 }
 
 # Output the Virtual Network ID

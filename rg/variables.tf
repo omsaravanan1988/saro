1b82aae90271f131d50c29334acd7e4695c4298e
omsaravanan1988 <91906734+omsaravanan1988@users.noreply.github.com>
2025-06-28 15:31:31 +0530

    Update variables.tf

--- rg/variables.tf
+++ rg/variables.tf
@@ -2,7 +2,7 @@ variable "vnetprefix" {
   description = "vnetrange"
   type = list (string)
   default = [ "10.0.0.0/16" ]
-
+}
 variable "subnetprefix" {
   description = "subnetrange"
   type = int
